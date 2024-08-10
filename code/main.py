f9e4019346c767981faec859be131aad81f9a981
0xlott <103859841+0xLott@users.noreply.github.com>
2024-08-10 09:16:32 +0000

    Fixed GraphQL queries

--- code/main.py
+++ code/main.py
@@ -1,32 +1,53 @@
 from dotenv import load_dotenv
-import os, requests
+import os
+import requests
 
 # Load .env variables and get GitHub API token
 load_dotenv()
 token = os.getenv('GITHUB_TOKEN')
 
 headers = {
-    "Authorization": f"Bearer {token}"
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
 }
 
 # GitHub GraphQL API endpoint
 url = "https://api.github.com/graphql"
 
 # Read GraphQL query files
-try:
-    with open('code/queries/all_issues_query.gql', 'r') as file:
-        all_issues = file.read()
-    with open('code/queries/all_issues_query.gql', 'r') as file:
-        closed_issues = file.read()
-except FileNotFoundError as e:
-    print(f"File not found error: {e}")
+with open("code/queries/all_issues_query.gql", "r") as file:
+    all_issues_query = file.read()
+with open("code/queries/closed_issues_query.gql", "r") as file:
+    closed_issues_query = file.read()
 
 # Send request
-response = requests.get(url, json={'query': closed_issues}, headers=headers)
+# response = requests.post(
+#     url,
+#     json={
+#         "query": closed_issues_query,
+#         "variables": {"num_repos": 50, "num_issues": 3}
+#     },
+#     headers=headers
+# )
+
+# if response.status_code == 200:
+#     data = response.json()
+#     print(data)
+# else:
+#     raise Exception(f"Failed to fetch repositories: {response.status_code}")
+
+# Send request
+response = requests.post(
+    url,
+    json={
+        "query": all_issues_query,
+        "variables": {"num_repos": 7, "num_issues": 100}
+    },
+    headers=headers
+)
 
 if response.status_code == 200:
     data = response.json()
     print(data)
 else:
     raise Exception(f"Failed to fetch repositories: {response.status_code}")
-
