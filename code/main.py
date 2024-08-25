8b28a17f9c13881ffc73bc4287047cacc091c488
Yan Nalon <havercraft1@gmail.com>
2024-08-24 23:32:12 -0300

    fix

--- code/main.py
+++ code/main.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
+import csv
 import os
 import requests
 
-# Load .env variables and get GitHub API token
 load_dotenv()
 token = os.getenv('GITHUB_TOKEN')
 
@@ -14,52 +14,105 @@ headers = {
 # GitHub GraphQL API endpoint
 url = "https://api.github.com/graphql"
 
-# Read GraphQL query files
-with open("code/queries/rq_06/all_issues_query.gql", "r") as file:
-    all_issues_query = file.read()
-with open("code/queries/rq_06/closed_issues_query.gql", "r") as file:
-    closed_issues_query = file.read()
+with open("queries/general_query.gql", "r") as file:
+    general_query = file.read()
+with open("queries/reduced_query.gql", "r") as file:
+    reduced_query = file.read()
 
-# POST request to the specified endpoint URL with a GraphQL query (read from file)
-# and headers, returning the response
-def run_query(query):
+
+def run_query(query, variables):
+    variables['cursor'] = ''
+    total_repos = variables['num_repos']
+    retrieved_data = []
+
+    def process_batch(batch_size):
+        variables['num_repos'] = batch_size
+        json = dispatch_request(query, variables).json()
+        display_data(json, retrieved_data)
+
+        variables['cursor'] = json['data']['search']['pageInfo']['endCursor']
+        return json
+
+    if total_repos > 100:
+        full_batches = total_repos // 100
+        remaining_items = total_repos % 100
+
+        for batch_number in range(full_batches):
+            print(f'Page {batch_number + 1}')
+            process_batch(100)
+
+        if remaining_items > 0:
+            print('Last Page:')
+            process_batch(remaining_items)
+    else:
+        process_batch(total_repos)
+
+    write_data(retrieved_data)
+
+
+def dispatch_request(query, variables):
     response = requests.post(
         url,
         json={
             "query": query,
-            "variables": {"num_repos": 1, "num_issues": 3}
+            "variables": variables
         },
         headers=headers
     )
+
     return response
 
-def display_data(response):
-    if response.status_code == 200:
-        data = response.json()
-        print("# All Issues:")
-        print(data)
-    else:
-        raise Exception(f"Failed to fetch repositories: {response.status_code}")
 
-# def aggregate_issues(response) {
-#     for edge in response['data']['search']['edges']:
-#         repository = edge['node']
-#         issues_count = len(repository['issues']['edges'])
-#         total_issues += issues_count
-#     return total_issues
-# }
+def get_unique_filename(base_path):
+    counter = 0
+    while True:
+        new_filename = f"{base_path.rsplit('.', 1)[0]}-{counter}.csv"
+        if not os.path.exists(new_filename):
+            return new_filename
+        counter += 1
 
-print(""" 
-    Select query:
-    1 - Closed issues
-    2 - All issues
-""")
 
-option = input("Option: ")
-match option:
-    case '1':
-        response = run_query(closed_issues_query)
-        display_data(response)
-    case '2':
-        response = run_query(all_issues_query)
-        display_data(response)
+def write_data(json):
+    csv_file_path = get_unique_filename('queries/results.csv')
+    headers = json[0].keys()
+
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(json)
+
+
+def display_data(response, retrieved_data):
+    for node in response['data']['search']['edges']:
+        print(node)
+        retrieved_data.append(node)
+
+
+def option_tree(option):
+    match option:
+        case '1':
+            variables = {
+                "num_repos": int(input("Repo Max Quantity: ")),
+                "num_issues": int(input("Issues Max Quantity: ")),
+                "issue_state": input("Issues State [OPEN, CLOSED]: ").upper(),
+                "num_pull_requests": int(input("Pull Requests Max Quantity: ")),
+                "num_releases": int(input("Releases Max Quantity: "))
+            }
+            run_query(general_query, variables)
+
+        case '2':
+            variables = {
+                "num_repos": int(input("Repo Max Quantity: ")),
+            }
+            run_query(reduced_query, variables)
+
+
+if __name__ == '__main__':
+    print(""" 
+        Select query:
+        1 - Complete
+        2 - Reduced
+    """)
+
+    option = input("Option: ")
+    option_tree(option)
