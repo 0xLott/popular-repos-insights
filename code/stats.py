12b2cd096c9c80fb22453fb0f5c329654052c7c4
0xlott <103859841+0xLott@users.noreply.github.com>
2024-08-31 06:11:42 +0000

    Metrics calculation (RQ_02)

--- code/stats.py
+++ code/stats.py
@@ -1,15 +1,19 @@
 from datetime import date, datetime, timezone
 import pandas as pd
+import ast
 import json
 
-data = pd.read_csv('queries/results/results-0.csv')
+basic_data = pd.read_csv('queries/results/results-0.csv')       # id, name, stars, createdAt, updatedAt, primaryLanguage
+issues_data = pd.read_csv('queries/results/results-1.csv')      # id, name, stars, createdAt, updatedAt, primaryLanguage, issues
+prs_data = pd.read_csv('queries/results/results-2.csv')         # id, name, stars, createdAt, updatedAt, primaryLanguage, pullRequests
+releases_data = pd.read_csv('queries/results/results-2.csv')    # id, name, stars, createdAt, updatedAt, releases, primaryLanguage
 
-# RQ_01:
-def rq_01(data):
+# RQ_01
+def rq_01(basic_data):
     repo_age = []
     today = datetime.today().date()
 
-    for created_at_date in data.createdAt:
+    for created_at_date in basic_data['createdAt']:
         date_obj = datetime.fromisoformat(created_at_date.replace('Z', '+00:00')).date()
         age_in_days = today - date_obj
         repo_age.append(age_in_days.days)
@@ -18,21 +22,30 @@ def rq_01(data):
     return created_at_median
 
 # RQ_02
-# merged_pull_requests_median
+def rq_02(prs_data):
+    merged_count = []
+
+    for pr in prs_data['pullRequests']:
+        pr_data = ast.literal_eval(pr)
+        merged_count.append(pr_data['totalCount'])
+    
+    merged_median = pd.Series(merged_count).median()
+    return merged_median
+
 
 # RQ_03
 # total_releases_median
 
 # RQ_04
-def rq_04(data):
+def rq_04(basic_data):
     minutes_since_update = []
     now = datetime.now(timezone.utc)
 
-    for last_updated_date in data.updatedAt:
+    for last_updated_date in basic_data['updatedAt']:
         date_obj = datetime.fromisoformat(last_updated_date.replace('Z', '+00:00'))
         time_difference = now - date_obj
 
-        minutes = time_difference.total_seconds() / 60 # Convert seconds to minutes
+        minutes = time_difference.total_seconds() / 60 # Seconds -> minutes
         minutes_since_update.append(minutes)
 
     updated_at_median = pd.Series(minutes_since_update).median()
